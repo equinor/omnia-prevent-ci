f56c3563ae1e1f3a7cb9c2eb247b3ce869c48b97
Roman Ivaniuk <rivan@equinor.com>
2023-01-24 14:49:02 +0200

    Fix: adjust imports inside scripts to local relative

--- scripts/reset_dev_branch.py
+++ scripts/reset_dev_branch.py
@@ -2,6 +2,6 @@
 
 from ghapi.actions import context_github
 
-from scripts.common import api
+from .common import api
 
 print(api.git.update_ref("heads/deploy/dev", sha=context_github.sha, force=True))
